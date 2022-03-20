cbafd8cdb2995e258bccd1c7c0101517396a541a
Lucas Araraki <araraki1@hotmail.com>
2022-03-19 21:39:34 -0300

    prepare for deploy

--- server/index.js
+++ server/index.js
@@ -6,3 +6,7 @@ const { port } = config
 
 server.listen(port)
   .on('listening', () => logger.info(`Server is running on ${port}`))
+
+// impede que um erro não tratado derrube a aplicação
+process.on('uncaughtException', (error) => logger.error(`UncaughtException happened: ${error.stack || error}`)) // throw
+process.on('unhandledRejection', (error) => logger.error(`UnhandledRejection happened: ${error.stack || error}`)) // Promises
